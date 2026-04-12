e9b46e41ae30343b3e31822ade1e8e2953c3a761
Giuseppe Petriccione <giuseppe.petriccione1996@gmail.com>
2026-04-12 19:47:23 +0200

    feat: enhance responsive design and improve layout for main page components

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,26 @@ import { Explorer } from "@/components/explorer";
 import { MainPage } from "@/components/main-page";
 import { Terminal } from "@/components/terminal";
 import "./page.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { sections } from "./contants";
 
 export default function Home() {
   const [activeFile, setActiveFile] = useState(sections[0]);
+  const [isCompactView, setIsCompactView] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)");
+    const updateViewMode = () => setIsCompactView(mediaQuery.matches);
+
+    updateViewMode();
+    mediaQuery.addEventListener("change", updateViewMode);
+
+    return () => {
+      mediaQuery.removeEventListener("change", updateViewMode);
+    };
+  }, []);
+
+  const selectedFileForMainPage = isCompactView ? sections[0] : activeFile;
 
   return (
     <main className="wrapper">
@@ -16,7 +31,7 @@ export default function Home() {
       <div className="editor">
         <Explorer activeFile={activeFile} onFileClick={setActiveFile} />
         <div className="main-column">
-          <MainPage selectedFile={activeFile} />
+          <MainPage selectedFile={selectedFileForMainPage} />
           <Terminal />
         </div>
       </div>
